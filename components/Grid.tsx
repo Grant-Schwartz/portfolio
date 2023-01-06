13df116bfe243b3f98be2de5310185d767ec3145
Grant Schwartz <grantwschwartz@gmail.com>
2023-01-06 12:33:24 -0500

    a bunch of changes

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,53 +1,374 @@
 import { ArrowForwardIcon } from "@chakra-ui/icons";
-import { Box, Center, Grid as ChakraGrid, GridItem, Heading, Image as ChakraImage, Link, Text, Flex } from "@chakra-ui/react";
-import Image from 'next/image';
+import {
+  Box,
+  Center,
+  Grid as ChakraGrid,
+  GridItem,
+  Heading,
+  Image as ChakraImage,
+  Link,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
+import Image from "next/image";
 import Head from "next/head";
 import { FC } from "react";
 
 export const Grid: FC = () => {
-    return (
-        <Flex flexDirection="column" alignItems="center" margin="20px">
-        <ChakraGrid
-            height={["800px", "800px" ,"400px", "400px"]}
-            width={["100%", "100%", "80%", "80%"]}
-            marginTop="20px"
-            templateRows={{ base: '1fr 1fr 1fr 1fr', sm: 'repeat(16, 1fr)', md: 'repeat(4, 1fr)' }}
-            templateColumns='repeat(4, 1fr)'
-            gap={4}
+  return (
+    <Flex flexDirection="column" alignItems="center" margin="20px">
+      <ChakraGrid
+        height={["800px", "800px", "400px", "400px"]}
+        width={["100%", "100%", "80%", "80%"]}
+        marginTop="20px"
+        templateRows={{
+          base: "1fr 1fr 1fr 1fr",
+          sm: "repeat(16, 1fr)",
+          md: "repeat(4, 1fr)",
+        }}
+        templateColumns="repeat(4, 1fr)"
+        gap={4}
+      >
+        <GridItem
+          rowSpan={[4, 4, 2, 2]}
+          colSpan={[2, 2, 1, 1]}
+          borderRadius="10px"
+          bgGradient="radial(130% 130% at 100% 100%, rgba(255, 255, 255, 0) 15%, blue.100 100%)"
+          padding="10px"
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _hover={{ transform: "scale(0.98)" }}
         >
-            <GridItem rowSpan={[4,4,2,2]} colSpan={[2, 2, 1, 1]} borderRadius="10px" bgGradient='radial(130% 130% at 100% 100%, rgba(255, 255, 255, 0) 15%, blue.100 100%)' padding="10px" transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _hover={{ transform: 'scale(0.98)' }}>
-                <Text fontSize={["18px","18px","18px","20px"]} fontWeight="550" lineHeight="25px" letterSpacing="-0.02em">👋 Hey! I&#39;m Grant. I&#39;m a coder, designer, entrepreneur, and student from <Text as="span" color="blue.500">NYC</Text>.</Text>
-            </GridItem>
-            <GridItem rowSpan={[8,8,4,4]} colSpan={[2, 2, 1, 1]} transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _hover={{ transform: 'scale(0.98)' }}>
-                <ChakraImage alt="an image of me" height="100%" width="100%" objectFit="cover" src="/me.jpeg" borderRadius="10px"/>
-            </GridItem>
-            <GridItem as={Link} href="/posts/flow-fantasy" data-group display="flex" rowSpan={[4,4,2,2]} colSpan={[2, 2, 1, 1]} borderRadius="10px" bgGradient='radial(100% 100% at 100% 0%, #ED64A6 0%, rgba(255, 255, 255, 0) 130%)' padding="10px" transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _hover={{ transform: 'scale(0.98)' }}>
-                <Text  fontSize={["18px","18px","18px","20px"]} fontWeight="550" lineHeight="25px" letterSpacing="-0.02em">Learn about how I built my first company, Flow Fantasy, and <Text as="span" color="pink.500">how I royally f*#cked it up</Text></Text>
-                <ArrowForwardIcon transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _groupHover={{ transform: 'rotate(-45deg)'}} h={6} w={6} alignSelf="end"/>
-            </GridItem>
-            <GridItem as={Link} href="/posts/the-ampersand" data-group display="flex" rowSpan={[4,4,2,2]} colSpan={[2, 2, 1, 1]} borderRadius="10px" bgGradient='radial(100% 100% at 0% 100%, purple.200 0%, rgba(255, 255, 255, 0) 100%)' padding="10px" transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _hover={{ transform: 'scale(0.98)' }}>
-                <Text  fontSize={["18px","18px","18px","20px"]} fontWeight="550" lineHeight="25px" letterSpacing="-0.02em">Learn a bit more about why I use <Text as="span" color="purple.500">the ampersand</Text></Text>
-                <ArrowForwardIcon transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _groupHover={{ transform: 'rotate(-45deg)'}} h={6} w={6} alignSelf="end"/>
-            </GridItem>
-            <GridItem rowSpan={[4,4,2,2]} colSpan={[2, 2, 1, 1]} borderRadius="10px" bgGradient='radial(120% 120% at 0% 100%, red.200 0%, rgba(255, 255, 255, 0) 100%)' padding="10px" transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _hover={{ transform: 'scale(0.98)' }}>
-                <Text fontSize={["18px","18px","18px","20px"]} fontWeight="550" lineHeight="25px" letterSpacing="-0.02em">I&#39;m currently a rising freshman at <Text as="span" color="red.500">Washington University in St. Louis.</Text></Text>
-            </GridItem>
-            <GridItem display="flex" flexDirection="column" justifyContent="space-between" rowSpan={[4,4,2,2]} colSpan={[2, 2, 1, 1]} borderRadius="10px" bgGradient='radial(100% 100% at 100% 100%, #48BB78 0%, rgba(255, 255, 255, 0) 100%)' padding="10px" transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _hover={{ transform: 'scale(0.98)' }}>
-                <Text fontSize={["18px","18px","18px","20px"]} fontWeight="550" lineHeight="25px" letterSpacing="-0.02em" color="green.400">I was unhappy with <Text as="span" color="black">pocket constitutions</Text>, so I made a new one. You can get it for free <Link color="black" _hover={{ textDecoration: 'none', opacity: '0.7' }} href="/posts/a-well-designed-constitution">here</Link></Text>
-                {/* <ArrowForwardIcon alignSelf="end" w={6} h={6} transform="rotate(-45deg)"/> */}
-            </GridItem>
-            <GridItem display="flex" flexDirection="column" justifyContent="space-between" rowSpan={[4,4,2,2]} colSpan={[2, 2, 1, 1]} borderRadius="10px" bgGradient='radial(120% 120% at 0% 0%, rgba(168, 181, 197, 0.91411) 0%, rgba(255, 255, 255, 0) 100%)' padding="10px" transition="all .2s cubic-bezier(0.8,0.04,0.4,1)" _hover={{ transform: 'scale(0.98)' }}>
-                <Text fontSize={["18px","18px","18px","20px"]} fontWeight="550" lineHeight="25px" letterSpacing="-0.02em" color="gray.700">Wanna learn more or get in touch?</Text>
-                <Box alignSelf="flex-end" display="flex" flexDirection="column" alignItems="end">
-                    <Link _hover={{ textDecoration: 'none', transform: "translateX(-10px)", opacity: "0.7" }} fontSize="20px" fontWeight="600" letterSpacing="-0.015em" marginBottom="-5px" href="https://github.com/Grant-Schwartz" target="_blank">GitHub</Link>
-                    <Link _hover={{ textDecoration: 'none', transform: "translateX(-10px)", opacity: "0.7" }} fontSize="20px" fontWeight="600" letterSpacing="-0.015em" marginBottom="-5px" href="https://twitter.com/GrantSchwartz17" target="_blank">Twitter</Link>
-                    <Link _hover={{ textDecoration: 'none', transform: "translateX(-10px)", opacity: "0.7" }} fontSize="20px" fontWeight="600" letterSpacing="-0.015em" margin="0px" href="https://www.linkedin.com/in/grant-w-schwartz/" target="_blank">LinkedIn</Link>
-
-
-                </Box>
-            </GridItem>
-            
-        </ChakraGrid>
-        </Flex>
-    );
-}
+          <Text
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+          >
+            👋 Hey! I&#39;m Grant. I&#39;m a coder, designer, entrepreneur, and
+            student from{" "}
+            <Text as="span" color="blue.500">
+              NYC
+            </Text>
+            .
+          </Text>
+        </GridItem>
+        <GridItem
+          rowSpan={[8, 8, 4, 4]}
+          colSpan={[2, 2, 1, 1]}
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _hover={{ transform: "scale(0.98)" }}
+        >
+          <ChakraImage
+            alt="an image of me"
+            height="100%"
+            width="100%"
+            objectFit="cover"
+            src="/me.jpeg"
+            borderRadius="10px"
+          />
+        </GridItem>
+        <GridItem
+          as={Link}
+          href="/posts/flow-fantasy"
+          data-group
+          display="flex"
+          rowSpan={[4, 4, 2, 2]}
+          colSpan={[2, 2, 1, 1]}
+          borderRadius="10px"
+          bgGradient="radial(100% 100% at 100% 0%, #ED64A6 0%, rgba(255, 255, 255, 0) 130%)"
+          padding="10px"
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _hover={{ transform: "scale(0.98)", textDecoration: "none" }}
+        >
+          <Text
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+          >
+            Learn about how I built my first company, Flow Fantasy, and{" "}
+            <Text as="span" color="pink.500">
+              how I royally messed it up
+            </Text>
+          </Text>
+          <ArrowForwardIcon
+            transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+            _groupHover={{ transform: "rotate(-45deg)" }}
+            h={6}
+            w={6}
+            alignSelf="end"
+          />
+        </GridItem>
+        {/* <GridItem
+          as={Link}
+          href="/posts/the-ampersand"
+          data-group
+          display="flex"
+          rowSpan={[4, 4, 2, 2]}
+          colSpan={[2, 2, 1, 1]}
+          borderRadius="10px"
+          bgGradient="radial(100% 100% at 0% 100%, purple.200 0%, rgba(255, 255, 255, 0) 100%)"
+          padding="10px"
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _hover={{ transform: "scale(0.98)" }}
+        >
+          <Text
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+          >
+            Learn a bit more about why I use{" "}
+            <Text as="span" color="purple.500">
+              the ampersand
+            </Text>
+          </Text>
+          <ArrowForwardIcon
+            transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+            _groupHover={{ transform: "rotate(-45deg)" }}
+            h={6}
+            w={6}
+            alignSelf="end"
+          />
+        </GridItem> */}
+        <GridItem
+          data-group
+          display="flex"
+          rowSpan={[4, 4, 2, 2]}
+          colSpan={[2, 2, 1, 1]}
+          borderRadius="10px"
+          bgGradient="radial(100% 100% at 0% 100%, purple.200 0%, rgba(255, 255, 255, 0) 100%)"
+          padding="10px"
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _hover={{ transform: "scale(0.98)" }}
+          flexDirection="column"
+        >
+          <Text
+            color="purple.500"
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+          >
+            Press
+          </Text>
+          <Link
+            target="blank"
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+            href="https://laxallstars.com/flow-fantasy-lacrosse/"
+            _hover={{
+              textDecoration: "underline",
+            }}
+          >
+            Lacrosse
+          </Link>
+          <Link
+            target="blank"
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+            href="https://open.spotify.com/episode/4avkuhfxkbaH2EZ892UU77"
+            _hover={{
+              textDecoration: "underline",
+            }}
+          >
+            Featured Podcast
+          </Link>
+          <Link
+            target="blank"
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+            href="https://www.bxtimes.com/bronx-student-nominated-for-usa-footballs-heart-of-a-giant-award/"
+            _hover={{
+              textDecoration: "underline",
+            }}
+          >
+            Football Award
+          </Link>
+          <Link
+            target="blank"
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+            href="https://www.youtube.com/watch?v=K42ZGXnWAmc)"
+            _hover={{
+              textDecoration: "underline",
+            }}
+          >
+            Award Winning Film
+          </Link>
+        </GridItem>
+        <GridItem
+          rowSpan={[4, 4, 2, 2]}
+          colSpan={[2, 2, 1, 1]}
+          borderRadius="10px"
+          bgGradient="radial(120% 120% at 0% 100%, red.200 0%, rgba(255, 255, 255, 0) 100%)"
+          padding="10px"
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _hover={{ transform: "scale(0.98)" }}
+        >
+          <Text
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+          >
+            I&#39;m currently a freshman at{" "}
+            <Text as="span" color="red.500">
+              Washington University in St. Louis.
+            </Text>
+          </Text>
+        </GridItem>
+        <GridItem
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          rowSpan={[4, 4, 2, 2]}
+          colSpan={[2, 2, 1, 1]}
+          borderRadius="10px"
+          bgGradient="radial(100% 100% at 100% 100%, #48BB78 0%, rgba(255, 255, 255, 0) 100%)"
+          padding="10px"
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _hover={{ transform: "scale(0.98)" }}
+        >
+          <Text
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+            color="green.400"
+          >
+            I was unhappy with{" "}
+            <Text as="span" color="black">
+              pocket constitutions
+            </Text>
+            , so I made a new one. You can get it for free{" "}
+            <Link
+              color="black"
+              _hover={{ textDecoration: "none", opacity: "0.7" }}
+              href="/posts/a-well-designed-constitution"
+            >
+              here
+            </Link>
+          </Text>
+          {/* <ArrowForwardIcon alignSelf="end" w={6} h={6} transform="rotate(-45deg)"/> */}
+        </GridItem>
+        <GridItem
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          rowSpan={[4, 4, 2, 2]}
+          colSpan={[2, 2, 1, 1]}
+          borderRadius="10px"
+          bgGradient="radial(120% 120% at 0% 0%, rgba(168, 181, 197, 0.91411) 0%, rgba(255, 255, 255, 0) 100%)"
+          padding="10px"
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _hover={{ transform: "scale(0.98)" }}
+        >
+          <Text
+            fontSize={["18px", "18px", "18px", "20px"]}
+            fontWeight="550"
+            lineHeight="25px"
+            letterSpacing="-0.02em"
+            color="gray.700"
+          >
+            Wanna learn more or get in touch?
+          </Text>
+          <Box
+            alignSelf="flex-end"
+            display="flex"
+            flexDirection="column"
+            alignItems="end"
+          >
+            <Link
+              _hover={{
+                textDecoration: "none",
+                transform: "translateX(-10px)",
+                opacity: "0.7",
+              }}
+              fontSize="20px"
+              fontWeight="600"
+              letterSpacing="-0.015em"
+              marginBottom="-5px"
+              href="/Resume.pdf"
+              target="_blank"
+            >
+              Resume
+            </Link>
+            <Link
+              _hover={{
+                textDecoration: "none",
+                transform: "translateX(-10px)",
+                opacity: "0.7",
+              }}
+              fontSize="20px"
+              fontWeight="600"
+              letterSpacing="-0.015em"
+              marginBottom="-5px"
+              href="https://github.com/Grant-Schwartz"
+              target="_blank"
+            >
+              GitHub
+            </Link>
+            <Link
+              _hover={{
+                textDecoration: "none",
+                transform: "translateX(-10px)",
+                opacity: "0.7",
+              }}
+              fontSize="20px"
+              fontWeight="600"
+              letterSpacing="-0.015em"
+              margin="0px"
+              href="https://www.linkedin.com/in/grant-w-schwartz/"
+              target="_blank"
+            >
+              LinkedIn
+            </Link>
+          </Box>
+        </GridItem>
+      </ChakraGrid>
+      <Box
+        height="100px"
+        marginTop="20px"
+        width={["100%", "100%", "80%", "80%"]}
+        padding="10px"
+        borderRadius="10px"
+        transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+        color="white"
+        display="flex"
+        justifyContent="space-between"
+        as={Link}
+        href="/posts/the-word"
+        bgGradient="radial(130% 130% at 0% 0%, #323E56 0%, rgba(255, 255, 255, 0) 100%)"
+        _hover={{ transform: "scale(0.98)", textDecoration: "none" }}
+        role="group"
+      >
+        <Text
+          fontSize={["18px", "18px", "18px", "20px"]}
+          fontWeight="550"
+          lineHeight="25px"
+          letterSpacing="-0.02em"
+          color="white"
+        >
+          I've been building a new company called the word! Learn more here.
+        </Text>
+        <ArrowForwardIcon
+          transition="all .2s cubic-bezier(0.8,0.04,0.4,1)"
+          _groupHover={{ transform: "rotate(-45deg)" }}
+          h={6}
+          w={6}
+          color="#242424"
+          alignSelf="end"
+        />
+      </Box>
+    </Flex>
+  );
+};
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,81 +1,100 @@
-import { getArticleFromSlug, getSlug } from "utils/mdx"
-import { serialize } from 'next-mdx-remote/serialize'
-import { MDXRemote } from 'next-mdx-remote'
-import rehypeHighlight from 'rehype-highlight'
-import rehypeCodeTitles from 'rehype-code-titles'
-import rehypeSlug from 'rehype-slug'
-import remarkRehype from 'remark-rehype'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import Head from 'next/head';
-import dayjs from "dayjs"
-import Image from 'next/image'
-import { Container, Text, Heading, Divider, Center, Box } from '@chakra-ui/react'
-import styles from 'styles/post.module.css';
-import { Nav } from "components/Nav"
-import { MDXComponents } from "components/MDXComponents"
+import { getArticleFromSlug, getSlug } from "utils/mdx";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import rehypeHighlight from "rehype-highlight";
+import rehypeCodeTitles from "rehype-code-titles";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import Head from "next/head";
+import dayjs from "dayjs";
+import Image from "next/image";
+import { Text, Heading, Divider, Center, Box } from "@chakra-ui/react";
+import styles from "styles/post.module.css";
+import { Nav } from "components/Nav";
+import { MDXComponents } from "components/MDXComponents";
 
 export default function Post({ post: { source, frontmatter } }: any) {
-    return (
-        <>
-        <Head>
-            <title>{frontmatter.title}</title>
-        </Head>
-        <Nav />
-        <div className={styles.article_container}>
-            {frontmatter.hero_image ? <Box  width="100%" height="200px" marginBottom="10px"  bgColor={`${frontmatter.color}.100`} position="relative" borderRadius="20px"><Center><Image alt="blog post image" src={frontmatter.hero_image} layout="fill" objectFit="contain"/></Center></Box> : null}
-            <Heading color={`${frontmatter.color}.500`}>{frontmatter.title}</Heading>
-            <Text color={`${frontmatter.color}.800`}>
-                {dayjs(frontmatter.publishedAt).format('MMMM D, YYYY')} &mdash;{' '}
-                {frontmatter.readingTime}
-            </Text>
-            <Divider marginTop="20px" marginBottom="20px"/>
-            <div className="content">
-                <MDXRemote {...source} components={MDXComponents} />
-            </div>
+  return (
+    <>
+      <Head>
+        <title>{frontmatter.title}</title>
+      </Head>
+      <Nav />
+      <div className={styles.article_container}>
+        {frontmatter.hero_image ? (
+          <Box
+            width="100%"
+            height="200px"
+            marginBottom="10px"
+            bgColor={`${frontmatter.color}.100`}
+            position="relative"
+            borderRadius="20px"
+          >
+            <Center>
+              <Image
+                alt="blog post image"
+                src={frontmatter.hero_image}
+                layout="fill"
+                objectFit="none"
+              />
+            </Center>
+          </Box>
+        ) : null}
+        <Heading color={`${frontmatter.color}.500`}>
+          {frontmatter.title}
+        </Heading>
+        <Text color={`${frontmatter.color}.800`}>
+          {dayjs(frontmatter.publishedAt).format("MMMM D, YYYY")} &mdash;{" "}
+          {frontmatter.readingTime}
+        </Text>
+        <Divider marginTop="20px" marginBottom="20px" />
+        <div className="content">
+          <MDXRemote {...source} components={MDXComponents} />
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export async function getStaticPaths() {
-    // getting all paths of each article as an array of
-    // objects with their unique slugs
-    const paths = (await getSlug()).map((slug) => ({ params: { slug } }))
-    return {
-      paths,
-      // in situations where you try to access a path
-      // that does not exist. it'll return a 404 page
-      fallback: false,
-    }
+  // getting all paths of each article as an array of
+  // objects with their unique slugs
+  const paths = (await getSlug()).map((slug) => ({ params: { slug } }));
+  return {
+    paths,
+    // in situations where you try to access a path
+    // that does not exist. it'll return a 404 page
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }: any) {
-    //fetch the particular file based on the slug
-    const { slug } = params
-    const { content, frontmatter } = await getArticleFromSlug(slug)
+  //fetch the particular file based on the slug
+  const { slug } = params;
+  const { content, frontmatter } = await getArticleFromSlug(slug);
 
-    const mdxSource = await serialize(content, {
-        mdxOptions: {
-        rehypePlugins: [
-            rehypeSlug,
-            [
-            rehypeAutolinkHeadings,
-            {
-                properties: { className: ['anchor'] },
-            },
-            { behaviour: 'wrap' },
-            ],
-            rehypeHighlight,
-            rehypeCodeTitles,
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: { className: ["anchor"] },
+          },
+          { behaviour: "wrap" },
         ],
-        },
-    })
-    return {
-        props: {
-        post: {
-            source: mdxSource,
-            frontmatter,
-        },
-        },
-    }
+        rehypeHighlight,
+        rehypeCodeTitles,
+      ],
+    },
+  });
+  return {
+    props: {
+      post: {
+        source: mdxSource,
+        frontmatter,
+      },
+    },
+  };
 }
